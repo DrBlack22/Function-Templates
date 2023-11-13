9544fd01fc1e1b989314c46a9ea96ca5415937f8
Blacky <97872836+DrBlack22@users.noreply.github.com>
2023-11-13 20:43:49 +0500

    First commit

---
+++ function templates.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+template <class ham>
+ham addShite(ham a,ham b){return a+b;}
+
+int main(){double x=7.6,y=43,z;
+z=addShite(x,y);
+cout << z << endl;}
+
+
+/* Functions that can handle multiple types of data
+
+- Template definition
+
+- Generic type of data
+
+- use that generic type of data to work with any kind
+of data with the same function
+
+-
+
+*/
